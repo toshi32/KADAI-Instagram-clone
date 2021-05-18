cb53c10587c5e53d655ec6a20a905a7cbce26b0a
toshi32 <viva.la.vida0522@gmail.com>
2021-05-19 01:05:40 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
-  get 'users/show'
-  get 'blogs/index'
-  get 'blogs/new'
-  get 'blogs/confirm'
-  get 'blogs/edit'
-  get 'blogs/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+    member do
+      patch :confirm
+    end
+  end
 end
