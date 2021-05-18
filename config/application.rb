4561a9c7e729e002d6ee984a05d52a1ddc324dfd
toshi32 <viva.la.vida0522@gmail.com>
2021-05-19 00:46:28 +0900

    タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module DiveIntoWork
   class Application < Rails::Application
-    # config.time_zone = 'Tokyo'
-    # config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
