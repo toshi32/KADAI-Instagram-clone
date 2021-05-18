19e5ca27fb07a47a867afc78bcc31436788b6cd4
toshi32 <viva.la.vida0522@gmail.com>
2021-05-19 00:55:58 +0900

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  get 'users/show'
+  get 'blogs/index'
+  get 'blogs/new'
+  get 'blogs/confirm'
+  get 'blogs/edit'
+  get 'blogs/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
