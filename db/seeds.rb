88e16b1bf8ee7186f33523d58d8c97508ca89881
toshi32 <viva.la.vida0522@gmail.com>
2021-05-19 22:28:32 +0900

    seed 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 50.times do |n|
-  name = Faker::Name.first_name
+  name = Faker::Name.name
   email = Faker::Internet.email
   password = "password"
-  User.create!(name: name,
+  User.create!( name: name,
                 email: email,
                 password: password,
-                password_confirmation: password,
-                )
+              )
 end
